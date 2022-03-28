f5f1ae6675211084e2707b8145932457f3d43df7
Anzar shah <61198659+anzarshah@users.noreply.github.com>
2022-03-28 06:56:53 +0530

    Update LB.java

--- LB.java
+++ LB.java
@@ -1,55 +1,55 @@
 import java.util.Scanner;
 import java.lang.*;
 
+
 public class LB{
 
+public static void main(String[] args){
+int drop=0;
+int cap,oprt,res;
+int cont,nsec;
+int ch;
+int x;
+int i=0;
+int[] inp = new int[25];
+Scanner sc = new Scanner(System.in);
+System.out.println("BUCKET SIZE");
+cap = sc.nextInt();
+System.out.println("OPRT FLOW");
+oprt = sc.nextInt();
 
-  public static void main(String[] args)
-  {
-   Scanner sc = new Scanner(System.in);
-   int cap,oprt,res;
-   int ch,cont,x,i=0,nsec;
-   int dp=0;
-   int[] inp = new int[25];
-
-   
-   
-   System.out.println("Enter the Bucket Size");
-   cap = sc.nextInt();
-   System.out.println("Enter the oprt ");
-   oprt = sc.nextInt();   
-   
-   do{
-     System.out.println("The packet at nsec "+ (i+1) );
-	 inp[i++] = sc.nextInt();
-     System.out.println("Do you wish to continue" );
-	 ch=sc.nextInt();
-    nsec=i;
-   }while(ch==1);
-   
-   System.out.println("\n :second\t\t:Recv\t\t:Sen\t\t:Bucket\t\t:dropped");
-   for(i=cont=0;(cont>0)||i<nsec;i++)
-   {
-   
-   System.out.print(+(i+1));
-   System.out.print("\t\t:"+inp[i]);
-   res = Math.min(cont+inp[i],oprt);
-   System.out.print("\t\t"+res);
-   if((x=cont+inp[i]-oprt)>0)
-   {
-     if(cont<cap){ cont=cap; dp=x-cap;}
-           else
-		     {cont=0;dp=0;}
-        }
-     else
-       {  cont=0; }
-   System.out.print("\t\t"+cont);
-   System.out.print("\t\t"+dp+"\n");
-   
-   
-   }
-   }
-   
+  do{
+   System.out.println("The inp at second flow "+(i+1));
+   inp[i++]=sc.nextInt();
   
-  }
+   System.out.println("Do you wish to continye ");
+     ch = sc.nextInt();
+   
 
+   }while(ch==1);
+	     nsec=i;
+   
+   System.out.println("SECOND /t Packet Recv\t Packet sent\tBucket\tDrop\t");
+   for(i=cont=0;(i<nsec)||(cont>0);i++)
+   {
+   System.out.print("\n"+(i+1));
+    System.out.print("\t"+inp[i]);
+	res=Math.min(inp[i]+cont,oprt);
+	System.out.print("\t"+res); 
+	if((x=cont+inp[i]-oprt)>0){
+		if(x>cap){ 
+		    cont = cap;
+			drop = x-cap;
+			 }
+		else{
+			cont=x;
+			drop =0;
+			}   		
+	}else {cont = 0;}
+	System.out.print("\t"+ cont);
+    System.out.print("\t"+ drop +"\n"); 
+   }
+
+
+}
+}
